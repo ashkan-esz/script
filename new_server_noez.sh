f15003a271be91d662fd66c9c0eb1c3aba38c4bf
ashkan-esz <58337265+ashkan-esz@users.noreply.github.com>
2024-06-07 13:09:24 +0330

    Create new_server_noez.sh

---
+++ new_server_noez.sh
@@ -0,0 +1,24 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo apt-get install unzip htop make docker-compose -y
+
+sudo useradd -m "amene"
+echo "amene:amene2828" | sudo chpasswd
+
+sudo useradd -m "mivechi"
+echo "mivechi:mivechi2828" | sudo chpasswd
+
+bash <(curl -Ls https://raw.githubusercontent.com/xpanel-cp/XPanel-SSH-User-Management/master/TCP-Tweaker --ipv4)
